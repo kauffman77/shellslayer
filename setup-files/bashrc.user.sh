b58046293d11cdb0903eae4b2dbbe7088ddf561e
Chris Kauffman <kauffman77@gmail.com>
2024-10-22 17:34:46 -0400

    Typo corrected in user init

--- setup-files/bashrc.user.sh
+++ setup-files/bashrc.user.sh
@@ -2,7 +2,8 @@
 
 export PS1='\w\$ '
 echo 'WELCOME!'
-echo 'You sense a fould presence in the process tree.'
+echo 'You sense a foul presence in the process tree....'
+echo ''
 echo 'A wild README.md appeared. Try typing:'
 echo '>> cat README.md'
 
