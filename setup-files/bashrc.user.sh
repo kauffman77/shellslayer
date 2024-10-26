49cac09759c58d4d8aa0c7eca135f52dd7643702
Chris Kauffman <kauffman77@gmail.com>
2024-10-25 21:47:06 -0400

    Added initial hints to bash prompt
    Hint on cat README.md added to prompt so that each time the prompt
    prints it shows; useful for after the daemon messes with the screen.

--- setup-files/bashrc.user.sh
+++ setup-files/bashrc.user.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/bash
 
-export PS1='\w\$ '
 echo 'WELCOME!'
 echo 'You sense a foul presence in the process tree....'
 echo ''
-echo 'A wild README.md appeared. Try typing:'
-echo '>> cat README.md'
+
+# started_file=~/.started
+# touch $started_file
+# && ! -z "$(find $started_file -mmin -2)" ]]; then
+
+# enable an initial hint on to get started
+export INITIAL_HINT=1
+
+initial_hint='A wild README.md appeared. Try typing:
+>> cat README.md
+Type `hint_off` to disable this hint
+'
+
+function hint_off {
+    initial_hint=""
+}
+
+export PS1='$initial_hint\w\$ '
 
 
