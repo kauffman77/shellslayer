d3bb0ecbfefcc1c006fd9fa64954c462aa2843c9
Chris Kauffman <kauffman77@gmail.com>
2024-10-27 15:10:34 -0400

    Added success / fail messages on exit

--- main.sh
+++ main.sh
@@ -3,6 +3,9 @@
 # # Start the first process
 # ./loop_sleep_print.sh 5 "Periodic Message" &
 
+DEBUG=1
+
+# perform basic setup in the setup-files directory
 cd setup-files
 
 mv bashrc.root.sh /root/.bashrc
@@ -10,28 +13,31 @@ useradd -s /usr/bin/bash -m -g users paladin
 # -s /usr/bin/bash : specify shell, bash preferred
 # -m : create a home directory for user
 # -g users : add to group "users"
-
 mv bashrc.user.sh /home/paladin/.bashrc
-
 python3 setup.py 12345
-rootpass=$(cat rootpass.txt)
-echo "root password is '$rootpass'"
-echo "root:$rootpass" | chpasswd
 chmod -R og-rwX .               # remove permissions on setup files
 
-cd /
+rootpass=$(cat rootpass.txt)    # grab root password from the file created during setup
+echo "root:$rootpass" | chpasswd
+
+if [[ ! -z "$DEBUG" ]]; then    # if debugging, show the root password
+    echo "root password is '$rootpass'"
+fi
+
+cd /                            # start the daemon process
 ./daemon &
 
-cd ~paladin
+cd ~paladin                     # change to user home 
 chown -R paladin .              # set permissions for whole home directory
-chgrp -R users .              # set group for whole home directory
-su paladin
+chgrp -R users .                # set group for whole home directory
+su paladin                      # give user control
 
-# # kill the periodic message
-# kill %1
+# ascii art by jgs
+# pass: https://www.asciiart.eu/food-and-drinks/drinks
+# fail: https://www.asciiart.eu/mythology/devils
 
-# # Wait for any process to exit
-# wait -n
-
-# # Exit with status of process that exited first
-# exit $?
+if [[ -z $(pgrep daemon) ]]; then
+    cat /setup-files/passmsg.txt
+else
+    cat /setup-files/failmsg.txt
+fi
